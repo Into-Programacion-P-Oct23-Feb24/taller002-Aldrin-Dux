eb053a7dd0ea0a8294540b96e72dc84cd1b3a359
Aldrin <apadilla2@utpl.edu.ec>
2023-10-19 16:50:10 -0500

    tercer cmmit, trabajdno con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basico de Java versionando a traves de git manejando desde GitHub ademas se usar GitHubClasroom");
+    }
+    
+}
